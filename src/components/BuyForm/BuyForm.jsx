832f208367972d1a9f30966385cc1c9110c0665e
Matias Godoy <matiasgodoy7@hotmail.com>
2022-02-23 11:20:54 -0300

    finished form logic

--- src/components/BuyForm/BuyForm.jsx
+++ src/components/BuyForm/BuyForm.jsx
@@ -2,30 +2,51 @@ import React, { useContext, useEffect, useState, useRef } from 'react';
 import { cartContext } from '../CartProvider/CartProvider';
 import firebase from 'firebase';
 import { getFirestore } from '../../firebase/firebase';
-import { Input, Button } from "reactstrap";
+import { Input, Button, Label } from "reactstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './BuyForm.css';
 
 const BuyForm = () => {
 
-    const { sumItems, cart, sumTotalPrice } = useContext(cartContext);
+    const { sumItems, cart, sumTotalPrice, removeCart } = useContext(cartContext);
     const [totalPrice, setTotalPrice] = useState(0);
     const [orderId, setOrderId] = useState("");
     const [booleanFlag, setBooleanFlag] = useState(false);
 
 
     const nameRef = useRef();
-    const nameBool = false;
+    const [nameBool, setNameBool] = useState(false);
 
     const addressRef = useRef();
+    const [addressBool, setAddressBool] = useState(false);
+
     const cityRef = useRef();
+    const [cityBool, setCityBool] = useState(false);
+
     const stateRef = useRef();
+    const [stateBool, setStateBool] = useState(false);
+
     const emailRef = useRef();
+    const [emailBool, setEmailBool] = useState(false);
+
     const mobileRef = useRef();
+    const [mobileBool, setMobileBool] = useState(false);
 
     useEffect(() => {
         setTotalPrice(sumTotalPrice());
     })
+
+    let booleanValidate = true;
+
+    const validate = (values, state) => {
+
+        if (values.current.value == "") {
+            return state(true);
+        } else {
+            state(false)
+        }
+    };
+
 
     function paymentButton() {
         const db = getFirestore();
@@ -54,15 +75,26 @@ const BuyForm = () => {
                     setOrderId(id);
                     console.log(orderId);
                     setBooleanFlag(false);
+                    setNameBool(false);
+                    setAddressBool(false);
+                    setCityBool(false);
+                    setStateBool(false);
+                    setEmailBool(false);
+                    setMobileBool(false);
                 })
                 .catch((err) => {
                     console.log('el error fue el siguiente: ' + err);
                 });
         } else {
-            return setBooleanFlag(true)
+            setBooleanFlag(true);
+            validate(nameRef, setNameBool);
+            validate(addressRef, setAddressBool);
+            validate(cityRef, setCityBool);
+            validate(stateRef, setStateBool);
+            validate(emailRef, setEmailBool);
+            validate(mobileRef, setMobileBool);
+
         }
-
-
     };
 
 
@@ -76,22 +108,95 @@ const BuyForm = () => {
                         Cantidad de productos: {sumItems}, por un valor total de: {totalPrice}
                     </h2>
                     <form>
-                        <Input type="text" innerRef={nameRef} name='name' placeholder="Nombre y apellido" />
+                        <Label for='name'>
+                            Escriba su nombre:
+                        </Label>
+                        <Input
+                            type="text"
+                            innerRef={nameRef}
+                            name='name'
+                            placeholder="Nombre y apellido"
+                        />
                         {(nameBool == true) ?
-                            <p>Ponga su nombre</p>
+                            <p>El casillero necesita estar completo.</p>
                             :
                             null
                         }
                         <br />
-                        <Input type='text' name='direccion' innerRef={addressRef} placeholder="Direccion" />
+                        <Label for='direccion'>
+                            Escriba su direccion:
+                        </Label>
+                        <Input
+                            type='text'
+                            name='direccion'
+                            innerRef={addressRef}
+                            placeholder="Direccion"
+                        />
+                        {(addressBool == true) ?
+                            <p>El casillero necesita estar completo.</p>
+                            :
+                            null
+                        }
                         <br />
-                        <Input type='text' name='ciudad' innerRef={cityRef} placeholder="Ciudad" />
+                        <Label for='ciudad'>
+                            Escriba la ciudad:
+                        </Label>
+                        <Input
+                            type='text'
+                            name='ciudad'
+                            innerRef={cityRef}
+                            placeholder="Ciudad"
+                        />
+                        {(cityBool == true) ?
+                            <p>El casillero necesita estar completo.</p>
+                            :
+                            null
+                        }
                         <br />
-                        <Input type='text' name='provincia' innerRef={stateRef} placeholder="Provincia" />
+                        <Label for='provincia'>
+                            Escriba su provincia:
+                        </Label>
+                        <Input
+                            type='text'
+                            name='provincia'
+                            innerRef={stateRef}
+                            placeholder="Provincia"
+                        />
+                        {(stateBool == true) ?
+                            <p>El casillero necesita estar completo.</p>
+                            :
+                            null
+                        }
                         <br />
-                        <Input type='text' name='email' innerRef={emailRef} placeholder="E-Mail" />
+                        <Label for='e-mail'>
+                            Escriba su e-mail:
+                        </Label>
+                        <Input
+                            type='text'
+                            name='email'
+                            innerRef={emailRef}
+                            placeholder="E-Mail"
+                        />
+                        {(emailBool == true) ?
+                            <p>El casillero necesita estar completo.</p>
+                            :
+                            null
+                        }
                         <br />
-                        <Input type='text' name='mobile' innerRef={mobileRef} placeholder="Numero de celular" />
+                        <Label for='numero de celular'>
+                            Escriba su numero de celular:
+                        </Label>
+                        <Input
+                            type='text'
+                            name='mobile'
+                            innerRef={mobileRef}
+                            placeholder="Numero de celular"
+                        />
+                        {(mobileBool == true) ?
+                            <p>El casillero necesita estar completo.</p>
+                            :
+                            null
+                        }
                     </form>
                     <Button variant="contained" color="primary" onClick={() => paymentButton()} >
                         Comprar
